33c7556a798ac38b7abed8a725760148bb685d72
Nicolas Betancur <nicobetancurochoa@gmail.com>
2024-02-07 23:09:56 -0500

    extra cambios

--- CMD.py
+++ CMD.py
@@ -2,10 +2,15 @@ import os
 import multiprocessing as Pool
 import subprocess as sub
 
+#def convert_to_acc(input_file, output_file):
+    #command to encode to acc.
+
 
 def convert_to_mp3(input_file, output_file):
     command = ['ffmpeg', '-i', input_file, '-codec:a', 'libmp3lame', '-qscale:a', '2', output_file]
     sub.run(command)
+    #change codec and bitrate.
+
 
 if __name__ == "__main__":
     path_folder = input("Enter the path folder:")
